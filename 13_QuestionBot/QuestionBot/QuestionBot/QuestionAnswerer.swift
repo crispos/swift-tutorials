7f87bbd0ccfb37da6ceb7eac0495140b467b5bad
cristian posadas <cristian.posadas01@gmail.com>
2019-02-05 14:02:44 -0600

    Completed chapter 13

--- 13_QuestionBot/QuestionBot/QuestionBot/QuestionAnswerer.swift
+++ 13_QuestionBot/QuestionBot/QuestionBot/QuestionAnswerer.swift
@@ -1,6 +1,16 @@
 struct MyQuestionAnswerer {
     func responseTo(question: String) -> String {
-        // TODO: Write a response 
-        return "?"
+        if question.lowercased() ==  "hello there" {
+            return "Why, Hello There!"
+        } else if question == "where are the cookies" {
+            return "In the cookie jar"
+            
+        } else if question.lowercased().hasPrefix("where") {
+            return "To the North!"
+        } else {
+            return "not the right answer"
+        }
+        
+        
     }
-}
+} 
